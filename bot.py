46128e9060140ba8ecd493427eac2795e89c25a3
cyrus <cyrusboushehri@airmail.cc>
2017-05-04 21:45:33 -0400

    Add multiple channels

--- bot.py
+++ bot.py
@@ -1,50 +1,39 @@
 #Cyrus Boushehri 4/5/17
-
 import socket
 
 server  = "localhost"                                          #Global settings
 port    = 6667
-channel = "#test"
+lang    = ["#test", "#test2"]
 botnick = "Tanobb"
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc     = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-ircsock.connect((server, port))                                #Connect to server
-ircsock.send(bytes("USER "+botnick+" "+botnick+" "+botnick+" "+botnick+"\n", "UTF-8"))
-ircsock.send(bytes("NICK "+botnick+"\n", "UTF-8"))
+irc.connect((server, port))                                    #Connect to server
+irc.send(bytes("USER "+botnick+" "+botnick+" "+botnick+" "+botnick+"\n", "UTF-8"))
+irc.send(bytes("NICK "+botnick+"\n", "UTF-8"))
 
-def joinchan(channel):                                         #Join Channel
-    ircsock.send(bytes("JOIN "+channel+"\n", "UTF-8"))
-
-    #ircmsg = ""
-    #while ircmsg.find("End of /NAMES list.") == -1:
-    #    ircmsg = ircsock.recv(2048).decode("UTF-8")
-    #    ircmsg = ircmsg.strip('\n\r')
-     #   print(ircmsg)
-
-def ping():                                                    #Respond to server Pings
-    ircsock.send(bytes("PONG :pingis\n", "UTF-8"))
-
-def sendmsg(msg, target=channel):                              #Send messages
-    ircsock.send(bytes("PRIVMSG "+ target +" :"+ msg +"\n", "UTF-8"))
+def sendmsg(msg, target):                                      #Send messages
+    irc.send(bytes("PRIVMSG "+target+" :"+msg+"\n", "UTF-8"))
 
 def translate(msg):                                            #Translation (placeholder)
     return msg
+
+for i in lang:                                                 #Join channels
+    irc.send(bytes("JOIN "+lang[0]+"\n", "UTF-8"))
+    irc.send(bytes("JOIN "+lang[1]+"\n", "UTF-8"))
     
-def main():
-    joinchan(channel)
+while 1:
+    ircmsg = irc.recv(2048).decode("UTF-8")                    #Get input
+    ircmsg = ircmsg.strip('\n\r')
     
-    while 1:
-        ircmsg = ircsock.recv(2048).decode("UTF-8")            #Strip formatting from input
-        ircmsg = ircmsg.strip('\n\r')
+    print(ircmsg) #debug
+    
+    if ircmsg.find("PING :") != -1:                            #Respond to server pings
+        irc.send(bytes("PONG :pingis\n", "UTF-8"))
         
-        print(ircmsg)
+    if ircmsg.find("PRIVMSG") != -1:                           #Listen for messages
+        name = ircmsg.split('!',1)[0][1:]
+        chan = ircmsg.split('#')[1].split(' :')[0]
+        msg  = ircmsg.split('PRIVMSG',1)[1].split(':',1)[1]
+        tr   = translate(msg)
         
-        if ircmsg.find("PING :") != -1:                        #Listen for pings
-            ping()
-        
-        if ircmsg.find("PRIVMSG") != -1:                       #Listen for messages
-            name    = ircmsg.split('!',1)[0][1:]
-            message = ircmsg.split('PRIVMSG',1)[1].split(':',1)[1]
-            message = translate(message)                       #Translate 
-            sendmsg("<"+name+"> "+message+"")
-main()
+        sendmsg("<"+name+"> "+tr+"", "#"+chan)
