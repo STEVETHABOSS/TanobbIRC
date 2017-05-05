d46520173acbab7108518a68f961d9be867daf28
cyrus <cyrusboushehri@airmail.cc>
2017-05-04 20:57:58 -0400

    Bot echos name and message. placeholder for translate function

--- bot.py
+++ bot.py
@@ -9,19 +9,17 @@ botnick = "Tanobb"
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ircsock.connect((server, port))                                #Connect to server
-
-ircsock.send(bytes("USER "+botnick+" "+botnick+" "+botnick+ " " +botnick+ "\n", "UTF-8")) #Bot information
+ircsock.send(bytes("USER "+botnick+" "+botnick+" "+botnick+" "+botnick+"\n", "UTF-8"))
 ircsock.send(bytes("NICK "+botnick+"\n", "UTF-8"))
 
-
 def joinchan(channel):                                         #Join Channel
-    ircsock.send(bytes("JOIN "+ channel +"\n", "UTF-8"))
+    ircsock.send(bytes("JOIN "+channel+"\n", "UTF-8"))
 
-    ircmsg = ""
-    while ircmsg.find("End of /NAMES list.") == -1:
-        ircmsg = ircsock.recv(2048).decode("UTF-8")
-        ircmsg = ircmsg.strip('\n\r')
-        print(ircmsg)
+    #ircmsg = ""
+    #while ircmsg.find("End of /NAMES list.") == -1:
+    #    ircmsg = ircsock.recv(2048).decode("UTF-8")
+    #    ircmsg = ircmsg.strip('\n\r')
+     #   print(ircmsg)
 
 def ping():                                                    #Respond to server Pings
     ircsock.send(bytes("PONG :pingis\n", "UTF-8"))
@@ -29,13 +27,16 @@ def ping():                                                    #Respond to serve
 def sendmsg(msg, target=channel):                              #Send messages
     ircsock.send(bytes("PRIVMSG "+ target +" :"+ msg +"\n", "UTF-8"))
 
+def translate(msg):                                            #Translation (placeholder)
+    return msg
+    
 def main():
     joinchan(channel)
     
     while 1:
-        
         ircmsg = ircsock.recv(2048).decode("UTF-8")            #Strip formatting from input
         ircmsg = ircmsg.strip('\n\r')
+        
         print(ircmsg)
         
         if ircmsg.find("PING :") != -1:                        #Listen for pings
@@ -44,16 +45,6 @@ def main():
         if ircmsg.find("PRIVMSG") != -1:                       #Listen for messages
             name    = ircmsg.split('!',1)[0][1:]
             message = ircmsg.split('PRIVMSG',1)[1].split(':',1)[1]
-            
-            print(name)
-            
-            sendmsg("" + name + "")
-            
-            #target = message.split(' ', 1)[1]
-            #if target.find(' ') != -1:
-            #    message = target.split(' ', 1)[1]
-            #    target = target.split(' ')[0]
-            
-            #sendmsg(message, target)
-                          
+            message = translate(message)                       #Translate 
+            sendmsg("<"+name+"> "+message+"")
 main()
